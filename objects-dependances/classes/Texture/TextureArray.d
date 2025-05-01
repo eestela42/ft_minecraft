c561e5c61043c5bd31283784c94622fff3e77cfc
eestela <augustinestela@gmail.com>
2025-05-01 20:33:50 +0200

    meh seems to be working, but problem appeared on the making of faces

--- objects-dependances/classes/Texture/TextureArray.d
+++
@@ -1,4 +0,0 @@
-objects-dependances/classes/Texture/TextureArray.o: \
- srcs/classes/Texture/TextureArray.cpp \
- includes/classes/Texture/TextureArray.hpp
-includes/classes/Texture/TextureArray.hpp:
